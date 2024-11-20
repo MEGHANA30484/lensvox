13d2b642d20d9c8022448554e475bbd93a0e1143
Meghana Singh <meghanasingh@Vaibhavs-Mac-mini.local>
2024-11-20 13:04:47 +0530

    changes done

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+        <Footer path="/Footer" element={<Footer/>} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
